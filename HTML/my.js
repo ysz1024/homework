e8960464e7b4b7e3485ff3d23d189bdbcec7057e
ysz1024 <ysz1024@qq.com>
2017-03-07 16:04:49 +0800

    3/7下午版本

--- HTML/my.js
+++ HTML/my.js
@@ -1,10 +1,31 @@
-function middle(){  
-    //获取屏幕的高度  
-    var heightBig=window.innerHeight;  
-    //获取元素的高度  
-    var d1=document. (".d1");  
-    var heightSmall=parseFloat(window.getComputedStyle(d1).height);  
-    d1.style.margin=(heightBig-heightSmall)/2+"px auto";  
-}  
-middle();  
-window.onresize=middle;  
+function getWindowSize(){
+	 var windowheight={
+	 	y:window.screen.availHeight
+		};
+	return windowheight;
+}
+
+function firstLoad(){
+	var size = getWindowSize()
+	var browseHeight = size.y;
+	var getDiv = document.getElementById('d').style.height;
+	var getDiv = getDiv.replace("px","");
+	var marginDistance = parseInt((browseHeight-getDiv)/2);
+	var marginDistance = marginDistance.toString()+"px";
+	document.getElementById('d').style.marginTop=marginDistance
+	console.log(window.screen.availHeight)
+	console.log(parseInt((browseHeight-getDiv)/2))
+
+}
+
+function getsize(){//浏览器窗口大小改变的时候
+    var size = getWindowSize();
+    var browseHeight=size.y;
+    var getDiv=document.getElementById('d').style.height;
+    var getDiv=getDiv.replace("px","");
+    var marginDistance=parseInt((browseHeight-getDiv)/2);
+    var marginDistance=marginDistance.toString()+"px";
+    document.getElementById('d').style.marginTop=marginDistance;
+}
+
+
